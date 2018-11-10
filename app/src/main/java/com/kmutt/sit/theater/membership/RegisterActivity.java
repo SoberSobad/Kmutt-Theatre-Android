88ef06125942ae5e9525455258123ce263f8528a
Maxine <night1714@hotmail.com>
2018-11-10 18:41:35 +0700

    Prepare for Login page decoration

--- app/src/main/java/com/kmutt/sit/theater/membership/RegisterActivity.java
+++ app/src/main/java/com/kmutt/sit/theater/membership/RegisterActivity.java
@@ -19,12 +19,32 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
 
         Spinner provinceDrop = findViewById(R.id.provinceSpin);
-        String[] provinces = new String[]{"1", "2", "three"};
+        String[] provinces = new String[]{"Amnat Charoen","Ang Thong","Bangkok","Bueng Kan"
+                + "Buriram","Chachoengsao","Chai Nat","Chaiyaphum"
+                + "Chanthaburi","Chiang Mai","Chiang Rai","Chonburi"
+                + "Chumphon","Kalasin","Kamphaeng Phet","Kanchanaburi"
+                + "Khon Kaen","Krabi","Lampang","Lamphun"
+                + "Loei","Lopburi","Mae Hong Son","Maha Sarakham"
+                + "Mukdahan","Nakhon Nayok","Nakhon Pathom","Nakhon Phanom"
+                + "Nakhon Ratchasima","Nakhon Sawan","Nakhon Si Thammarat","Nan"
+                + "Narathiwat","Nong Bua Lam Phu","Nong Khai","Nonthaburi"
+                + "Pathum Thani","Pattani","Phang Nga","Phatthalung"
+                + "Phayao","Phetchabun","Phetchaburi","Phichit"
+                + "Phitsanulok","Phra Nakhon Si Ayutthaya","Phrae","Phuket"
+                + "Prachinburi","Prachuap Khiri Khan","Ranong","Ratchaburi"
+                + "Rayong","Roi Et","Sa Kaeo","Sakon Nakhon"
+                + "Samut Prakan","Samut Sakhon","Samut Songkhram","Saraburi"
+                + "Satun","Sing Buri","Sisaket","Songkhla"
+                + "Sukhothai","Suphan Buri","Surat Thani","Surin"
+                + "Tak","Trang","Trat","Ubon Ratchathani"
+                + "Udon Thani","Uthai Thani","Uttaradit","Yala"
+                + "Yasothon"};
         ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, provinces);
         provinceDrop.setAdapter(adapter);
 
         Spinner genderDrop = findViewById(R.id.genderSpin);
         String[] genders = new String[]{"Male", "Female"};
+        //ArrayAdapter<String> genderSpinAdapt = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, genders);
         ArrayAdapter<String> genderSpinAdapt = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, genders);
         genderDrop.setAdapter(genderSpinAdapt);
     }
