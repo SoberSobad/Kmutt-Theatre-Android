03223ce0fbe85990c58e093243f486778789dbb4
Maxine <night1714@hotmail.com>
2018-11-10 17:50:18 +0700

    Update 17:50

--- app/src/main/java/com/kmutt/sit/theater/membership/RegisterActivity.java
+++ app/src/main/java/com/kmutt/sit/theater/membership/RegisterActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.Spinner;
 
 import com.kmutt.sit.theater.MainActivity;
 import com.kmutt.sit.theater.R;
@@ -16,6 +18,14 @@ public class RegisterActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        Spinner provinceDrop = findViewById(R.id.provinceSpin);
+        String[] provinces = new String[]{"1", "2", "three"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, provinces);
+        provinceDrop.setAdapter(adapter);
 
+        Spinner genderDrop = findViewById(R.id.genderSpin);
+        String[] genders = new String[]{"Male", "Female"};
+        ArrayAdapter<String> genderSpinAdapt = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, genders);
+        genderDrop.setAdapter(genderSpinAdapt);
     }
 }
