29d9984c18377c3d40541939fdb597064fe64b35
freshthit <fresh.thit@me.com>
2018-12-18 16:40:40 +0700

    [SEAT] seat selection now works

--- app/src/main/java/com/kmutt/sit/theater/booking/SeatActivity.java
+++ app/src/main/java/com/kmutt/sit/theater/booking/SeatActivity.java
@@ -2,6 +2,7 @@ package com.kmutt.sit.theater.booking;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -37,6 +38,16 @@ public class SeatActivity extends AppCompatActivity {
 
         ZoomableImageView imageView = (ZoomableImageView) findViewById(R.id.zoomable_image);
         SeatExample seats[][] = new SeatExample[10][10];
+        for (int i = 0; i < seats.length; i++) {
+            for (int j = 0; j < seats[i].length; j++) {
+                SeatExample s = new SeatExample();
+                s.id = (seats[i].length * i) + j;
+                s.status = HallScheme.SeatStatus.FREE;
+                s.selectedSeatMarker = String.valueOf(i+1);
+                Log.d("SeatMap debug", "id = " + s.id + ", color = " + s.color + ", status = " + s.status);
+                seats[i][j] = s;
+            }
+        }
         HallScheme scheme = new HallScheme(imageView, seats, this);
 
         scheme.setSeatListener(new SeatListener() {
